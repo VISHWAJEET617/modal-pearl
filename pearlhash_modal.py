c10013e2e81a066ca65f8702a587d1516578ccce
Vishwajeet kumar <beautysinha1188@gmail.com>
2026-05-31 10:54:03 +0530

    Update pearlhash_modal.py

--- pearlhash_modal.py
+++ pearlhash_modal.py
@@ -8,7 +8,7 @@ import modal
 
 app = modal.App("pearlhash-miner")
 
-WALLET = "CHANGE_YOUR_MINER_ADDRESS"
+WALLET = "prl1p2xc96paswys3urvqgz3gq7uc2sl8nmrcvgqcquc7yylt7wypwelqd6e499"
 POOL_HOST = "84.32.220.219:9000"
 WORKER = "modal-a100"
 
